c5ad8e1112fafc9f2047cac39947ccaeb94f60e0
BenZen59 <benoit.zenone@gmail.com>
2023-04-03 12:11:01 +0200

    add redux file and create store

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from '../Header/Header';
+import { connect } from 'react-redux';
+import * as actionTypes from '../../store/actionType';
 
 export default function MovieCard() {
   const [dataFilms, setDataFilms] = useState([]);
+  const [favoris, setFavoris] = useState([]);
+
   useEffect(() => {
     axios
       .get(
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './store/reducer';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
     <App />
   </React.StrictMode>
 );
